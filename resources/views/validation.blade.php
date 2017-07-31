6c79e6480f2e7263b2ff874043f2fc1dfa5c0afc
Ernande Junior <ernande.junior@gmail.com>
2017-07-31 14:10:58 -0300

    Adicionando validacao de conta

--- resources/views/validation.blade.php
+++ resources/views/validation.blade.php
@@ -3,7 +3,7 @@
 </head>
 <body>
 	<p>Obrigado por se cadastrar em nosso site {{$name}}, clique no link abaixo para ativar sua conta</p>
-	<a href="http://mysterious-crag-46869.herokuapp.com/validation/{{$validationToken}}">Clique aqui</a>
+	<a href="https://afternoon-hollows-81019.herokuapp.com/validation/{{$validationToken}}">Clique aqui</a>
 	<br>
 	<p>Obrigado.</p>
 </body>
